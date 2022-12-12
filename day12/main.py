0aa4d3e5362ddfe53b1b0ee2eed055105204b52a
luw5008 <37829455+LukasW352435@users.noreply.github.com>
2022-12-12 08:38:11 +0100

    Day 12

---
+++ day12/main.py
@@ -0,0 +1,68 @@
+import sys
+print(sys.getrecursionlimit())
+sys.setrecursionlimit(2000)
+
+
+def walk_to(y, x, steps):
+    global grid_steps
+    global grid
+    current = grid[y][x]
+    if grid_steps[y][x] > steps:
+        grid_steps[y][x] = steps
+    if grid_steps[y][x] < steps:
+        return
+    if current == '{':
+        return
+    if y - 1 >= 0:
+        new = grid[y - 1][x]
+        if ord(new) <= ord(current) + 1 and is_better(y, x, y - 1, x):
+            walk_to(y - 1, x, steps + 1)
+    if y + 1 < len(grid):
+        new = grid[y + 1][x]
+        if ord(new) <= ord(current) + 1 and is_better(y, x, y + 1, x):
+            walk_to(y + 1, x, steps + 1)
+    if x - 1 >= 0:
+        new = grid[y][x - 1]
+        if ord(new) <= ord(current) + 1 and is_better(y, x, y, x - 1):
+            walk_to(y, x - 1, steps + 1)
+    if x + 1 < len(grid[0]):
+        new = grid[y][x + 1]
+        if ord(new) <= ord(current) + 1 and is_better(y, x, y, x + 1):
+            walk_to(y, x + 1, steps + 1)
+    pass
+
+
+def is_better(y, x, ny, nx):
+    if grid_steps[ny][nx] > grid_steps[y][x] + 1:
+        return True
+    return False
+
+
+ys = 0
+xs = 0
+
+ye = 0
+xe = 0
+
+grid_steps = []
+
+with open("input.txt") as f:
+    txt = f.read().split('\n')
+    for i in range(0, len(txt)):
+        txt[i] = list(txt[i])
+
+    grid_steps = [[99999] * len(txt[0]) for i in range(0, len(txt))]
+
+    for i in range(0, len(txt)):
+        for j in range(0, len(txt[0])):
+            if txt[i][j] == 'S':
+                ys = i
+                xs = j
+            if txt[i][j] == 'E':
+                ye = i
+                xe = j
+    txt[ys][xs] = 'a'
+    txt[ye][xe] = '{'
+    grid = txt
+    walk_to(ys, xs, 0)
+    print(grid_steps[ye][xe])
